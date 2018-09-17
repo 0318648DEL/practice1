57accb9dba5cccbab47a579166475a4ebf5e0023
재범 서 <gleldzm3212@kpu.ac.kr>
2018-09-18 04:22:59 +0900

    6번 시도

--- practiceeeeeeeeeee/pracccccc6.cpp
+++ practiceeeeeeeeeee/pracccccc6.cpp
@@ -9,6 +9,8 @@
 /*끼이ㅏ럼ㄴ이럼;ㅣ나얼
 하기 싫지만 해야 한다 과제니까 겜수 과제 하나 잊어버려서 화가 난다 그래서 한다고 시바
 히든 문제라 추가된게 매우 까다로운거 같다 귀찮은게 잔뜩
+시계나 반시계 방향 회전은 함수로 땜빵하고 p는 무슨 소린지 모르겠다
+내일 물어봐야지
 */
 
 GLvoid drawScene(GLvoid);
@@ -17,6 +19,13 @@ void mouse(int button, int state, int x, int y);
 void keyboard(unsigned char key, int x, int y);
 void timer_func(int value);
 void rand_dir_init(int x, int y);
+void turn_clockwise(int squr_num);
+void go_right(int squr_num);
+void go_left(int squr_num);
+void go_down(int squr_num);
+void go_up(int squr_num,int normal);
+void go_right_back(int squr_num, int normal);
+
 
 int squr_loc[10][2] = { 0, };//사각형 중앙 좌표
 int size_x[10] = { 0, };//크기 x쪽으로
@@ -44,6 +53,7 @@ void main(int argc, char** argv)
 	glutKeyboardFunc(keyboard);
 	glutDisplayFunc(drawScene);
 	glutTimerFunc(100, timer_func, 1);
+	//glutTimerFunc(100, turn_clockwise, 2);
 	glutReshapeFunc(reshape);
 	glutMainLoop();
 }
@@ -54,7 +64,7 @@ GLvoid drawScene()
 	glClear(GL_COLOR_BUFFER_BIT);//뭐였지 이거
 	int count = 0;
 
-	while (count < 10 && trigger[count] == 1)//색깔 바꾸는 부분과 사각형 그리기 부분
+	while (count < 9 && trigger[count] == 1)//색깔 바꾸는 부분과 사각형 그리기 부분
 	{
 		glColor4ub(color[count][0], color[count][1], color[count][2], 200);//색깔 그리긴데 내가 봤던 거랑 좀 다르게 나온다. 안 예쁨
 		glRecti(squr_loc[count][0] - size_x[count], squr_loc[count][1] - size_y[count], squr_loc[count][0] + size_x[count], squr_loc[count][1] + size_y[count]);//사각형이다
@@ -95,6 +105,56 @@ void keyboard(unsigned char key, int x, int y)//키보드 입력받음
 			++check;
 		}
 		break;
+	case 's':
+		while (trigger[check] == 1)
+		{
+			if (speed_mul[check] > 0)
+			{
+				speed_mul[check] = 0;
+			}
+			++check;
+			break;
+		}
+	case '0':
+		turn_clockwise(0);
+		rand_dir_init(rand_dir_x[0], rand_dir_y[0]);
+		break;
+	case '1':
+		turn_clockwise(1);
+		rand_dir_init(rand_dir_x[1], rand_dir_y[1]);
+		break;
+	case '2':
+		turn_clockwise(2);
+		rand_dir_init(rand_dir_x[2], rand_dir_y[2]);
+		break;
+	case '3':
+		turn_clockwise(3);
+		rand_dir_init(rand_dir_x[3], rand_dir_y[3]);
+		break;
+	case '4':
+		turn_clockwise(4);
+		rand_dir_init(rand_dir_x[4], rand_dir_y[4]);
+		break;
+	case '5':
+		turn_clockwise(5);
+		rand_dir_init(rand_dir_x[5], rand_dir_y[5]);
+		break;
+	case '6':
+		turn_clockwise(6);
+		rand_dir_init(rand_dir_x[6], rand_dir_y[6]);
+		break;
+	case '7':
+		turn_clockwise(7);
+		rand_dir_init(rand_dir_x[7], rand_dir_y[7]);
+		break;
+	case '8':
+		turn_clockwise(8);
+		rand_dir_init(rand_dir_x[8], rand_dir_y[8]);
+		break;
+	case '9':
+		turn_clockwise(9);
+		rand_dir_init(rand_dir_x[9], rand_dir_y[9]);
+		break;
 	}
 	glutPostRedisplay();//??다시 그리기??
 }
@@ -133,10 +193,73 @@ void rand_dir_init(int x, int y)
 	y = (-20 + ((rand() % 10) + 5));
 }
 
+void turn_clockwise(int squr_num)
+{
+	int normal_pos_x = squr_loc[squr_num][0];
+	int normal_pos_y = squr_loc[squr_num][1];
+
+	go_right(squr_num);
+	go_down(squr_num);
+	go_left(squr_num);
+	go_up(squr_num, normal_pos_y);
+	go_right_back(squr_num, normal_pos_x);
+}
+
+void go_right(int squr_num)
+{
+	while (squr_loc[squr_num][0] <= 800)
+	{
+		rand_dir_x[squr_num] = 10;
+		rand_dir_y[squr_num] = 0;
+		glutPostRedisplay();
+		glutTimerFunc(100, timer_func, 1);//타이머 호출 다시
+	}
+}
+
+void go_right_back(int squr_num, int normal)
+{
+	if (squr_loc[squr_num][0] <= normal)
+	{
+		rand_dir_x[squr_num] = 10;
+		rand_dir_y[squr_num] = 0;
+		glutTimerFunc(100, timer_func, 1);//타이머 호출 다시
+	}
+}
+
+void go_left(int squr_num)
+{
+	if (squr_loc[squr_num][0] >= 0)
+	{
+		rand_dir_x[squr_num] = -10;
+		rand_dir_y[squr_num] = 0;
+		glutTimerFunc(100, timer_func, 1);//타이머 호출 다시
+	}
+}
+
+void go_up(int squr_num, int normal)
+{
+	if (squr_loc[squr_num][1] >= normal)
+	{
+		rand_dir_x[squr_num] = 0;
+		rand_dir_y[squr_num] = -10;
+		glutTimerFunc(100, timer_func, 1);//타이머 호출 다시
+	}
+}
+
+void go_down(int squr_num)
+{
+	if (squr_loc[squr_num][1] <= 600)
+	{
+		rand_dir_x[squr_num] = 0;
+		rand_dir_y[squr_num] = 10;
+		glutTimerFunc(100, timer_func, 1);//타이머 호출 다시
+	}
+}
+
 void timer_func(int value)//타이머 호출
 {
 	int timer = 0;//1~10까지 변수 안 만들어도 될거 같은데 만듦
-	while (timer < 10)//10까지 돈다
+	while (timer < 9)//10까지 돈다
 	{
 		if (squr_loc[timer][0] <= 0 || squr_loc[timer][1] <= 0 || squr_loc[timer][0] >= 800 || squr_loc[timer][1] >= 600)//x좌표는 0과 800, y좌표는 0과 600까지 이 중에 하나라도 걸리면 바로 아래로
 		{//이게 아마 튕기는 부분으로 만들었을껄 좌표 체크랑 튕기는거
@@ -233,7 +356,7 @@ void timer_func(int value)//타이머 호출
 			size_x[timer] -= 5;
 			size_y[timer] -= 5;
 			--size_ch[timer];
-			rev == 1;//다시 뒤로 간다
+			rev = 1;//다시 뒤로 간다
 			break;
 		}
 		++timer;
