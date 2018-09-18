af26c87e0286ce3099b55fa381f743415926ef2b
재범 서 <gleldzm3212@kpu.ac.kr>
2018-09-18 21:36:17 +0900

    6완성

--- practiceeeeeeeeeee/pracccccc6.cpp
+++ practiceeeeeeeeeee/pracccccc6.cpp
@@ -19,11 +19,10 @@ void mouse(int button, int state, int x, int y);
 void keyboard(unsigned char key, int x, int y);
 void timer_func(int value);
 void rand_dir_init(int x, int y);
-void turn_clockwise(int squr_num);
 void go_right(int squr_num);
 void go_left(int squr_num);
 void go_down(int squr_num);
-void go_up(int squr_num,int normal);
+void go_up(int squr_num, int normal);
 void go_right_back(int squr_num, int normal);
 
 
@@ -38,7 +37,10 @@ int speed_mul[10] = { 1, 1,1,1,1,1,1,1,1,1 };//속도 배수
 int rev = 0;//크기 줄어드는거 체크
 int num = 0;//사각형 카운트
 int trigger[10] = { 0, };//토리가
-
+int turn_clockwise[10] = { 0, };
+int turned[10] = { 0, };
+int normal_loc_x[10] = { 0, };
+int normal_loc_y[10] = { 0, };
 
 
 void main(int argc, char** argv)
@@ -83,6 +85,7 @@ GLvoid reshape(int w, int h)
 void keyboard(unsigned char key, int x, int y)//키보드 입력받음
 {
 	int check = 0;//검사검사
+	int all = 0;
 	switch (key)//입력받은 키
 	{
 	case '+'://q의 경우 증가
@@ -106,54 +109,88 @@ void keyboard(unsigned char key, int x, int y)//키보드 입력받음
 		}
 		break;
 	case 's':
-		while (trigger[check] == 1)
+		while (trigger[check] == 1&&check<9)
 		{
-			if (speed_mul[check] > 0)
+			if (speed_mul[check] >= 0)
 			{
 				speed_mul[check] = 0;
 			}
 			++check;
-			break;
 		}
+		break;
+	case 'p':
+		while (trigger[check] == 1 && check < 9)
+		{
+			rand_dir_x[check] = (rand() % 30) - 15;
+			rand_dir_y[check] = (rand() % 30) - 15;
+			++check;
+		}
+		break;
 	case '0':
-		turn_clockwise(0);
-		rand_dir_init(rand_dir_x[0], rand_dir_y[0]);
+		turn_clockwise[0] = 1;
+		all = 0;
+		while (all < 9)
+		{
+			turn_clockwise[all + 1] = 1;
+			normal_loc_x[all] = squr_loc[all][0];
+			normal_loc_y[all] = squr_loc[all][1];
+			go_right(all);
+			++all;
+		}
 		break;
 	case '1':
-		turn_clockwise(1);
-		rand_dir_init(rand_dir_x[1], rand_dir_y[1]);
+		turn_clockwise[1] = 1;
+		normal_loc_x[0] = squr_loc[0][0];
+		normal_loc_y[0] = squr_loc[0][1];
+		go_right(0);
 		break;
 	case '2':
-		turn_clockwise(2);
-		rand_dir_init(rand_dir_x[2], rand_dir_y[2]);
+		turn_clockwise[2] = 1;
+		normal_loc_x[1] = squr_loc[1][0];
+		normal_loc_y[1] = squr_loc[1][1];
+		go_right(1);
 		break;
 	case '3':
-		turn_clockwise(3);
-		rand_dir_init(rand_dir_x[3], rand_dir_y[3]);
+		turn_clockwise[3] = 1;
+		normal_loc_x[2] = squr_loc[2][0];
+		normal_loc_y[2] = squr_loc[2][1];
+		go_right(2);
 		break;
 	case '4':
-		turn_clockwise(4);
-		rand_dir_init(rand_dir_x[4], rand_dir_y[4]);
+		turn_clockwise[4] = 1;
+		normal_loc_x[3] = squr_loc[3][0];
+		normal_loc_y[3] = squr_loc[3][1];
+		go_right(3);
 		break;
 	case '5':
-		turn_clockwise(5);
-		rand_dir_init(rand_dir_x[5], rand_dir_y[5]);
+		turn_clockwise[5] = 1;
+		normal_loc_x[4] = squr_loc[4][0];
+		normal_loc_y[4] = squr_loc[4][1];
+		go_right(4);
 		break;
 	case '6':
-		turn_clockwise(6);
-		rand_dir_init(rand_dir_x[6], rand_dir_y[6]);
+		turn_clockwise[6] = 1;
+		normal_loc_x[5] = squr_loc[5][0];
+		normal_loc_y[5] = squr_loc[5][1];
+		go_right(5);
 		break;
 	case '7':
-		turn_clockwise(7);
-		rand_dir_init(rand_dir_x[7], rand_dir_y[7]);
+		turn_clockwise[7] = 1;
+		normal_loc_x[6] = squr_loc[6][0];
+		normal_loc_y[6] = squr_loc[6][1];
+		go_right(6);
 		break;
 	case '8':
-		turn_clockwise(8);
-		rand_dir_init(rand_dir_x[8], rand_dir_y[8]);
+		turn_clockwise[8] = 1;
+		normal_loc_x[7] = squr_loc[7][0];
+		normal_loc_y[7] = squr_loc[7][1];
+		go_right(7);
 		break;
 	case '9':
-		turn_clockwise(9);
-		rand_dir_init(rand_dir_x[9], rand_dir_y[9]);
+		turn_clockwise[9] = 1;
+		normal_loc_x[8] = squr_loc[8][0];
+		normal_loc_y[8] = squr_loc[8][1];
+		go_right(8);
 		break;
 	}
 	glutPostRedisplay();//??다시 그리기??
@@ -172,7 +209,8 @@ void mouse(int button, int state, int x, int y)
 			squr_loc[num][1] = y;//y좌표
 			size_x[num] += (rand() % 20) + 5;
 			size_y[num] += (rand() % 10) + 5;
-			rand_dir_init(rand_dir_x[num], rand_dir_y[num]);
+			rand_dir_x[num] = (rand() % 30) - 15;
+			rand_dir_y[num] = (rand() % 30) - 15;
 		}
 		else
 		{
@@ -180,88 +218,74 @@ void mouse(int button, int state, int x, int y)
 			squr_loc[num][1] = y;
 			size_x[num] += (rand() % 20) + 5;
 			size_y[num] += (rand() % 10) + 5;
-			rand_dir_init(rand_dir_x[num], rand_dir_y[num]);
+			rand_dir_x[num] = (rand() % 30) - 15;
+			rand_dir_y[num] = (rand() % 30) - 15;
 		}
 		++num;
 	}
 	glutPostRedisplay();//??다시 그리기??
 }
 
-void rand_dir_init(int x, int y)
-{
-	x = (-20 + ((rand() % 10) + 5));
-	y = (-20 + ((rand() % 10) + 5));
-}
-
-void turn_clockwise(int squr_num)
-{
-	int normal_pos_x = squr_loc[squr_num][0];
-	int normal_pos_y = squr_loc[squr_num][1];
-
-	go_right(squr_num);
-	go_down(squr_num);
-	go_left(squr_num);
-	go_up(squr_num, normal_pos_y);
-	go_right_back(squr_num, normal_pos_x);
-}
+//void rand_dir_init(int x, int y)
+//{
+//	x = (-20 + ((rand() % 10) + 5));
+//	y = (-20 + ((rand() % 10) + 5));
+//}
 
 void go_right(int squr_num)
 {
-	while (squr_loc[squr_num][0] <= 800)
-	{
-		rand_dir_x[squr_num] = 10;
-		rand_dir_y[squr_num] = 0;
-		glutPostRedisplay();
-		glutTimerFunc(100, timer_func, 1);//타이머 호출 다시
-	}
+
+	rand_dir_x[squr_num] = 10;
+	rand_dir_y[squr_num] = 0;
+
 }
 
-void go_right_back(int squr_num, int normal)
-{
-	if (squr_loc[squr_num][0] <= normal)
-	{
-		rand_dir_x[squr_num] = 10;
-		rand_dir_y[squr_num] = 0;
-		glutTimerFunc(100, timer_func, 1);//타이머 호출 다시
-	}
-}
-
-void go_left(int squr_num)
-{
-	if (squr_loc[squr_num][0] >= 0)
-	{
-		rand_dir_x[squr_num] = -10;
-		rand_dir_y[squr_num] = 0;
-		glutTimerFunc(100, timer_func, 1);//타이머 호출 다시
-	}
-}
-
-void go_up(int squr_num, int normal)
-{
-	if (squr_loc[squr_num][1] >= normal)
-	{
-		rand_dir_x[squr_num] = 0;
-		rand_dir_y[squr_num] = -10;
-		glutTimerFunc(100, timer_func, 1);//타이머 호출 다시
-	}
-}
-
-void go_down(int squr_num)
-{
-	if (squr_loc[squr_num][1] <= 600)
-	{
-		rand_dir_x[squr_num] = 0;
-		rand_dir_y[squr_num] = 10;
-		glutTimerFunc(100, timer_func, 1);//타이머 호출 다시
-	}
-}
+//void go_right_back(int squr_num, int normal)
+//{
+//	if (squr_loc[squr_num][0] <= normal)
+//	{
+//		rand_dir_x[squr_num] = 10;
+//		rand_dir_y[squr_num] = 0;
+//		glutTimerFunc(100, timer_func, 1);//타이머 호출 다시
+//	}
+//}
+//
+//void go_left(int squr_num)
+//{
+//	if (squr_loc[squr_num][0] >= 0)
+//	{
+//		rand_dir_x[squr_num] = -10;
+//		rand_dir_y[squr_num] = 0;
+//		glutTimerFunc(100, timer_func, 1);//타이머 호출 다시
+//	}
+//}
+//
+//void go_up(int squr_num, int normal)
+//{
+//	if (squr_loc[squr_num][1] >= normal)
+//	{
+//		rand_dir_x[squr_num] = 0;
+//		rand_dir_y[squr_num] = -10;
+//		glutTimerFunc(100, timer_func, 1);//타이머 호출 다시
+//	}
+//}
+//
+//void go_down(int squr_num)
+//{
+//	if (squr_loc[squr_num][1] <= 600)
+//	{
+//		rand_dir_x[squr_num] = 0;
+//		rand_dir_y[squr_num] = 10;
+//		glutTimerFunc(100, timer_func, 1);//타이머 호출 다시
+//	}
+//}
 
 void timer_func(int value)//타이머 호출
 {
 	int timer = 0;//1~10까지 변수 안 만들어도 될거 같은데 만듦
 	while (timer < 9)//10까지 돈다
 	{
-		if (squr_loc[timer][0] <= 0 || squr_loc[timer][1] <= 0 || squr_loc[timer][0] >= 800 || squr_loc[timer][1] >= 600)//x좌표는 0과 800, y좌표는 0과 600까지 이 중에 하나라도 걸리면 바로 아래로
+		if ((squr_loc[timer][0] <= 0 || squr_loc[timer][1] <= 0 || squr_loc[timer][0] >= 800 || squr_loc[timer][1] >= 600) && turn_clockwise[timer + 1] == 0)//x좌표는 0과 800, y좌표는 0과 600까지 이 중에 하나라도 걸리면 바로 아래로
 		{//이게 아마 튕기는 부분으로 만들었을껄 좌표 체크랑 튕기는거
 			if (squr_loc[timer][0] <= 0)
 			{
@@ -284,6 +308,38 @@ void timer_func(int value)//타이머 호출
 		color[timer][0] = rand() % 250 + 5;//야호
 		color[timer][1] = rand() % 250 + 5;//색깔
 		color[timer][2] = rand() % 250 + 5;//변환
+
+		if (turn_clockwise[timer + 1] == 1)
+		{
+			if (squr_loc[timer][0] >= 800 && squr_loc[timer][1] <= 600)
+			{
+				rand_dir_x[timer] = 0;
+				rand_dir_y[timer] = 10;
+			}
+			else if (squr_loc[timer][1] >= 600 && squr_loc[timer][0] >= 0)
+			{
+				rand_dir_x[timer] = -10;
+				rand_dir_y[timer] = 0;
+			}
+			else if (squr_loc[timer][0] <= 0 && squr_loc[timer][1] >= normal_loc_y[timer])
+			{
+				rand_dir_x[timer] = 0;
+				rand_dir_y[timer] = -10;
+				turned[timer] = 1;
+			}
+			else if (squr_loc[timer][1] <= normal_loc_y[timer] && squr_loc[timer][0] <= normal_loc_x[timer])
+			{
+				rand_dir_x[timer] = 10;
+				rand_dir_y[timer] = 0;
+			}
+			else if (turned[timer] == 1)
+			{
+				rand_dir_x[timer] = (rand() % 30) - 15;
+				rand_dir_y[timer] = (rand() % 30) - 15;
+				turn_clockwise[timer + 1] = 0;
+				turned[timer] = 0;
+			}
+		}
 
 		squr_loc[timer][0] += (rand_dir_x[timer] * speed_mul[timer]);
 		squr_loc[timer][1] += (rand_dir_y[timer] * speed_mul[timer]);
